d48b1f7ab479b3064ccf8ae4f3dc91afe649fff9
tanhathung24 <hungtn.19@grad.uit.edu.vn>
2026-01-13 18:09:18 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCa3rOr2qJ4eDt_qAltXaY3sPXn_cBdrZU",
+  authDomain: "chi-tieu-49fe5.firebaseapp.com",
+  projectId: "chi-tieu-49fe5",
+  storageBucket: "chi-tieu-49fe5.firebasestorage.app",
+  messagingSenderId: "1018225594634",
+  appId: "1:1018225594634:web:0220af13e0c050aaab2af3",
+  measurementId: "G-8X0HQQYJNH"
+};
